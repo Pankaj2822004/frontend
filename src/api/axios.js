3ef5fb93c1bf1a22901e9d4ec08a71d46f720b42
Pankaj Pandey <pankajpandey2822003@gmail.com>
2025-09-24 17:07:16 +0530

    Updated axios baseURL with env variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,7 @@
 import axios from "axios";
-require("dotenv").config({ path: "./" });
+// require("dotenv").config({ path: "./" });
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_UR
+  baseURL: process.env.REACT_APP_API_URL + "/api",
+});
+
+export default API;
