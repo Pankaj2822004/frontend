9c2bf00ed1f23303a3d0a805ca2433cadb96e7f1
Pankaj Pandey <pankajpandey2822003@gmail.com>
2025-09-24 20:57:38 +0530

    split

--- api/models/Room.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const RoomSchema = new mongoose.Schema({
-    members:{
-        type:[String]
-    }
-});
-
-module.exports = mongoose.model('Room', RoomSchema);
